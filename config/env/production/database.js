65d871bfc8d985cb1b58c33ca0939fb55a5024c6
Maarten Louage <me@maartenlouage.nl>
2024-10-14 15:40:57 +0200

    Update environment variables

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', env('POSTGRES_HOST')),
       port: env.int('DATABASE_PORT', env('POSTGRES_PORT')),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
+      database: env('DATABASE_NAME', env('POSTGRES_DATABASE')),
+      user: env('DATABASE_USERNAME', env('POSTGRES_USERNAME')),
+      password: env('DATABASE_PASSWORD', env('POSTGRES_PASSWORD')),
       ssl: env.bool('DATABASE_SSL', false)
     }
   }
