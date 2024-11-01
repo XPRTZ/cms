b3833cbae864ae7f23a022c9a0673b7af56fe4f7
Maarten Louage <me@maartenlouage.nl>
2024-11-01 13:49:00 +0100

    Azure upload

--- config/plugins.js
+++ config/plugins.js
@@ -12,18 +12,20 @@ module.exports = ({ env }) => ({
   },
   upload: {
     config: {
-      provider: 'aws-s3',
+      provider: "@Physer/strapi-provider-upload-azure-storage",
       providerOptions: {
-        credentials: {
-          accessKeyId: env('MINIO_ACCESS_KEY'),
-          secretAccessKey: env('MINIO_SECRET_KEY'),
-        },
-        region: 'eu-central-1',
-        baseUrl: `http://localhost:9000/${env('MINIO_BUCKET')}`,
-        endpoint: env('MINIO_ENDPOINT'),
-        params: {
-          Bucket: env('MINIO_BUCKET'),
-        },
+        authType: env("STORAGE_AUTH_TYPE", "default"),
+        account: env("STORAGE_ACCOUNT"),
+        accountKey: env("STORAGE_ACCOUNT_KEY"),//either account key or sas token is enough to make authentication
+        sasToken: env("STORAGE_ACCOUNT_SAS_TOKEN"),
+        serviceBaseURL: env("STORAGE_URL"), // optional
+        containerName: env("STORAGE_CONTAINER_NAME"),
+        createContainerIfNotExist: env("STORAGE_CREATE_CONTAINER_IF_NOT_EXIST", 'false'), // optional
+        publicAccessType: env("STORAGE_PUBLIC_ACCESS_TYPE"), // optional ('blob' | 'container')
+        defaultPath: "assets",
+        cdnBaseURL: env("STORAGE_CDN_URL"), // optional
+        defaultCacheControl: env("STORAGE_CACHE_CONTROL"), // optional
+        removeCN: env("REMOVE_CONTAINER_NAME"), // optional, if you want to remove container name from the URL
       },
     },
   },
