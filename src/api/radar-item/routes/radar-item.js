a4745cf37794c7c561b639a88c7396cdd1af93e2
Dick van Hirtum <dick@xprtz.net>
2025-12-20 10:50:30 +0100

    added technology radar components

---
+++ src/api/radar-item/routes/radar-item.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * radar-item router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::radar-item.radar-item');
